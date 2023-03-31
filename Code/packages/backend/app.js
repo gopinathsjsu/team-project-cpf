2d7258abc99100f109c33ec45511860199bd89a2
mvschiranjeevi <mvschiranjeevi@gmail.com>
2023-03-31 14:14:50 -0700

    login details

--- Code/packages/backend/app.js
+++ Code/packages/backend/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 var connection = require("./database");
+var usersRouter = require("./routes/users");
+var authRoutes = require("./routes/auth");
 
 // database connection
 connection();
@@ -12,7 +14,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/location", locationRoutes);
-
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRoutes);
 
 const port = 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));

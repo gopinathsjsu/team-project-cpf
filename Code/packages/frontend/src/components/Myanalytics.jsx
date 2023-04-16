0d65c148c3803ad60b312401928a72fc1b8677bb
Sakethreddy0909 <sakethreddy.awala@sjsu.edu>
2023-04-16 00:09:10 -0700

    analytics completion

--- Code/packages/frontend/src/components/Myanalytics.jsx
+++ Code/packages/frontend/src/components/Myanalytics.jsx
@@ -1,23 +1,76 @@
-import { Button ,Box} from "@chakra-ui/react";
+import { Button ,Box, Heading} from "@chakra-ui/react";
 import React,{useState,useEffect} from "react";
 import { Bar } from "react-chartjs-2";
 import { Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import axios from 'axios';
 
+import { Select } from "@chakra-ui/react";
 
-
-function MyButton(props) {
+function MyComponent(props) {
+    const locations = props.locations
+    console.log(locations);
   return (
-    <Button colorScheme="blue" size="md" {...props}>
-      {props.children}
-    </Button>
+    <Select size="sm" variant="filled" colorScheme="blue">
+      {locations.map((lc) => (
+        <option key={lc._id}>
+          {lc.location}
+        </option>
+      ))}
+    </Select>
   );
 }
+
+
+
 const Myanalytics = () => {
     const [activities, setActivities] = useState([]);
     const [hoursbyweek, setHoursByWeek]=useState({"Sunday":0,"Monday":0, "Tuesday":0,"Wednesday":0,"Thursday":0,"Friday":0,"Saturday":0});
     const [visitorsbytime, setVisitorsByTime] = useState({"Last Day":0,"Last Week":0,"Last Month":0});
+    const [activitiesByUsers,setActivitiesByUsers] = useState({});
+    const [equipmentmap, setEquipmentmap] = useState({});
+    const [locationMap, setLocationMap] = useState({});
+    const getLocations = async () => {
+        const url = "http://localhost:8080/api/location";
+        let lc = await axios.get(url);
+        lc = lc.data;
+        lc.map((item)=>{
+            locationMap[item._id] = item.locations;
+        });
+        console.log(lc);
+        setLocationMap(lc);
+    }
+    function getDaysOrder(){
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const today = new Date().getDay();
+        console.log("------->",today);
+        for(var idx=6; idx > today; idx--){
+            var el = days.pop();
+            days = [el, ...days]
+        }
+        return days
+    }
+    // const [daybyweek, setDayByweek]=useState({"Sunday":0,"Monday":0, "Tuesday":0,"Wednesday":0,"Thursday":0,"Friday":0,"Saturday":0})
+    // const getClass= async()=>{
+    //     const url="http://localhost:8080/api/class";
+    //     const data  = await axios.get(url);
+    //     const activityObjectList = data.data;
+    //     let currentUserId = localStorage.getItem("token");
+    //     currentUserId = currentUserId ? JSON.parse(localStorage.getItem("token")).data._id : undefined;
+    //     let userActivity=activityObjectList;
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const getEquipments=async()=>{
+        const url="http://localhost:8080/api/equipment";
+        let eq=await axios.get(url);
+        eq = eq.data;
+        eq.map((item)=>{
+            equipmentmap[item._id] = item.name
+            console.log(item._id, item.name);
+    });
+        console.log(equipmentmap);
+      setEquipmentmap(equipmentmap);
+    }
     const getActivities = async () => {
         const url = "http://localhost:8080/api/activity";
         const data  = await axios.get(url);
@@ -26,9 +79,9 @@ const Myanalytics = () => {
         currentUserId = currentUserId ? JSON.parse(localStorage.getItem("token")).data._id : undefined;
         let userActivity = activityObjectList; //activityObjectList.filter((obj) => obj.userId === currentUserId);
         console.log(userActivity)
-        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        // const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-        
+        let tmp3={"Sunday":0,"Monday":0, "Tuesday":0,"Wednesday":0,"Thursday":0,"Friday":0,"Saturday":0}
 
         let a = [];
         let tmp = {"Sunday":0,"Monday":0, "Tuesday":0,"Wednesday":0,"Thursday":0,"Friday":0,"Saturday":0};
@@ -59,12 +112,40 @@ const Myanalytics = () => {
                 let et = new Date(`2000-01-01T${item.endTime}:00`);
                 tmp[daysOfWeek[(date.getDay()+1)%7]] += ((et.getTime() - st.getTime()) / (1000 * 60 * 60));
             }
-
+            
 
         });
         console.log(tmp);
         setActivities(a);
         setHoursByWeek(tmp);
+        let tmp4 = {};
+        userActivity.map((item)=>{
+            // within this week
+            const now = new Date();
+            const date = new Date(item.date);
+            const difference = now.getTime() - date.getTime();
+            const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+            if (days <= 7 && days >=0){
+                let st = new Date(`2000-01-01T${item.startTime}:00`);
+                let et = new Date(`2000-01-01T${item.endTime}:00`);
+                console.log("|||||||---->",item,equipmentmap[item.equipmentId], tmp4);
+                if (equipmentmap[item.equipmentId] in tmp4){
+                    tmp4[equipmentmap[item.equipmentId]][daysOfWeek[(date.getDay()+1)%7]] += 1;                     
+                }else{
+                    let tmp = {}
+                    let days = getDaysOrder();
+                    days.map((day)=>{
+                        tmp[day] = 0
+                    })
+                    tmp4[equipmentmap[item.equipmentId]] = tmp;
+                    tmp4[equipmentmap[item.equipmentId]][daysOfWeek[(date.getDay()+1)%7]] = 1;
+                }
+            }
+            console.log(tmp4);
+            setActivitiesByUsers(tmp4);
+        });
+
+
     };
 
 
@@ -72,13 +153,15 @@ const Myanalytics = () => {
 
 
         useEffect(() => {
+            getEquipments();
             getActivities();
+            getLocations()
           }, []);
     const userActivityData = {
         labels: Object.keys(visitorsbytime),
         datasets: [
           {
-            label: 'User activity by location',
+            label: 'User activity by day/week/month',
             data: Object.values(visitorsbytime),
             backgroundColor: 'rgba(54, 162, 235, 0.5)',
             borderColor: 'rgba(54, 162, 235, 1)',
@@ -91,15 +174,15 @@ const Myanalytics = () => {
         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         datasets: [
           {
-            label: 'Classes',
-            data: [50, 30, 40, 60, 80, 70, 90],
+            label: 'Rowing',
+            data: [5, 10, 15, 20, 25, 30, 35,40],
             backgroundColor: 'rgba(255, 99, 132, 0.5)',
             borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1,
           },
           {
-            label: 'Enrollment',
-            data: [30, 20, 35, 45, 55, 60, 70],
+            label: 'Treadmill',
+            data: [5, 10, 15, 20, 25, 30, 35,40],
             backgroundColor: 'rgba(75, 192, 192, 0.5)',
             borderColor: 'rgba(75, 192, 192, 1)',
             borderWidth: 1,
@@ -107,12 +190,13 @@ const Myanalytics = () => {
         ],
       };
 
+        
     //   const LABELS = hoursbyweek.keys
     //   console.log(hoursbyweek);
       const gymHoursData = {
         // console.log(hoursbyweek.keys());
 
-        labels:  Object.keys(hoursbyweek),
+        labels:  getDaysOrder(),
         datasets: [
           {
             label: 'Hours spent in the gym',
@@ -123,7 +207,56 @@ const Myanalytics = () => {
           },
         ],
       };
-    
+      function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+
+      function putdata(activitiesByUsers){
+        let datasets = [];
+        for(let activity in activitiesByUsers){
+            let ob = {
+                label: activity,
+                data : Object.values(activitiesByUsers[activity]),
+                borderColor: getRandomColor(),
+                borderWidth: 1
+            }
+            datasets.push(ob);
+        }
+        return datasets;
+      }
+      console.log(putdata(activitiesByUsers));
+
+
+      const data_ = {
+        labels: getDaysOrder(),
+        datasets : putdata(activitiesByUsers)
+        // datasets: [
+        //   {
+        //     label: 'Line 1',
+        //     data: [3, 2, 5, 4, 6, 7, 8],
+        //     borderColor: 'red',
+        //     borderWidth: 1,
+        //   },
+        //   {
+        //     label: 'Line 2',
+        //     data: [5, 6, 4, 7, 8, 9, 10],
+        //     borderColor: 'blue',
+        //     borderWidth: 1,
+        //   },
+        // ],
+      };
+    // const data_ = data.map(({ label, values }) => ({
+    //     label,
+    //     data: values,
+    //     borderColor: getRandomColor(), // optional styling options
+    //     fill: false,
+    //   }));
+
       const visitorsData = {
         labels: [
           '00:00',
@@ -167,29 +300,37 @@ const Myanalytics = () => {
       };
   
   return (
-    <Box padding={"5%"}>
-  <h1>View analytics dashboard</h1>
 
+    <Box padding={"5%"}>
+  <Heading as="h2" size="lg" mb={4} textAlign="center">
+        View analytics dashboard
+    </Heading>
+<MyComponent locations = {Object.values(locationMap)}/>
   <div style={{ display: 'flex' }}>
     <div style={{ width: '50%' }}>
-      <h2>User activity summarized by location</h2>
+    <Heading as="h2" size="md" mb={4} textAlign="center">
+        User activity summarized by day/week/month    
+    </Heading>
       <Bar data={userActivityData} />
     </div>
-    <div style={{ width: '50%' }}>
-      <h2>Classes and enrollment by day/week</h2>
-      <Line data={enrollmentData} />
+    <div style={{ width: '50%', marginLeft: "4%" }}>
+    <Heading as="h2" size="md" mb={4} textAlign="center">
+        Classes and enrollment by day/week
+    </Heading>
+
+      <Line data={data_} />
     </div>
   </div>
 
-  <div style={{ display: 'flex' }}>
-    <div style={{ width: '50%' }}>
-      <h2>Hours spent in the gym by day/week/month</h2>
+  <div style={{ display: 'flex', marginLeft:"7%" }}>
+    <div style={{ width: '100%' }}>
+    <Heading as="h2" size="md" mb={4} textAlign="center">
+        Hours spent in the gym by day/week/month
+    </Heading>
+
       <Line data={gymHoursData} />
     </div>
-    <div style={{ width: '50%' }}>
-      <h2>Number of visitors by the hour each day, weekday, weekend</h2>
-      <Line data={visitorsData} />
-    </div>
+    
   </div>
 </Box>
 
